85f136ec6a196509068004ecf824a386f4ec5f82
Captain-Kim <brighthero@kakao.com>
2024-06-14 04:37:08 +0900

    update: 로그아웃 상태에서는 헤더 렌더링 하지 않도록 조건부 렌더링 설정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Header from "./Header";
+import { useLocation } from "react-router-dom";
 
 const StyleDiv = styled.div`
     max-width: 1280px;
@@ -7,9 +8,12 @@ const StyleDiv = styled.div`
 `
 
 function Layout({ children }) {
+    const location = useLocation();
+    const isLoginPage = location.pathname === "/login";
+
     return (
         <>
-            <Header />
+            {!isLoginPage && <Header />}
             <StyleDiv>{children}</StyleDiv>
         </>
     );
