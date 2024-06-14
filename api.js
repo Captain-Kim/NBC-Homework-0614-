285bd284907dead35fb60bed82af0aa19dfe1e8f
Captain-Kim <brighthero@kakao.com>
2024-06-14 12:18:51 +0900

    feat: json server 글리치 서버로 변경

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const jsonApi = axios.create({
-    baseURL: 'http://localhost:5011',
+    baseURL: 'https://living-lily-gold.glitch.me',
     timeOUT: 5000,
     headers: {'Content-Type': 'application/json'}
 })
